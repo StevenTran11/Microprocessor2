38a3e4cc447859eab585f084578d34f8d1df24a5
StevenTran11 <98437341+StevenTran11@users.noreply.github.com>
2022-02-03 22:56:58 -0500

    Update testCode.ino

--- lab1/testCode/testCode.ino
+++ lab1/testCode/testCode.ino
@@ -40,7 +40,7 @@ void setup()
   byte digitPins[] = {2, 3, 4, 5};
   byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
   bool resistorsOnSegments = false; // 'false' means resistors are on digit pins
-  byte hardwareConfig = COMMON_ANODE; // See README.md for options
+  byte hardwareConfig = COMMON_CATHODE; // See README.md for options
   bool updateWithDelays = false; // Default 'false' is Recommended
   bool leadingZeros = false; // Use 'true' if you'd like to keep the leading zeros
   bool disableDecPoint = true; // Use 'true' if your decimal point doesn't exist or isn't connected
@@ -97,11 +97,16 @@ void loop()
         remainder = (limit - (millis() - startTime)) / 1000;
         if(remainder <= 65535  && remainder >= 0)
         {
-          //Serial.println(remainder);
+          Serial.println(remainder);
           sevseg.setNumber(remainder, -1, true);
+        }
+        if(remainder < 3)
+        {
+          digitalWrite(buzzer, HIGH);
         }
         sevseg.refreshDisplay();
       }
+      digitalWrite(buzzer, LOW);
       digitalWrite(red, LOW);
       digitalWrite(green,HIGH);
       digitalWrite(yellow, LOW);
@@ -112,11 +117,16 @@ void loop()
         remainder = (limit - (millis() - startTime)) / 1000;
         if(remainder <= 65535 && remainder >= 0)
         {
-          //Serial.println(remainder);
+          Serial.println(remainder);
           sevseg.setNumber(remainder, -1, true);
+        }
+        if(remainder < 3)
+        {
+          digitalWrite(buzzer, HIGH);
         }
         sevseg.refreshDisplay();
       }
+      digitalWrite(buzzer, LOW);
       digitalWrite(red, LOW);
       digitalWrite(green, LOW);
       digitalWrite(yellow, HIGH);
@@ -127,11 +137,16 @@ void loop()
         remainder = (limit - (millis() - startTime)) / 1000;
         if(remainder <= 65535 && remainder >= 0)
         {
-          //Serial.println(remainder);
+          Serial.println(remainder);
           sevseg.setNumber(remainder, -1, true);
+        }
+        if(remainder < 3)
+        {
+          digitalWrite(buzzer, HIGH);
         }
         sevseg.refreshDisplay();
       }
+      digitalWrite(buzzer, LOW);
     }
   }
 }
