da892eec0a71b9d1a7df0b9f918aa01cbbf146a6
StevenTran11 <98437341+StevenTran11@users.noreply.github.com>
2022-02-10 05:28:33 -0500

    LED with non blink timer

--- lab1/continuous_timerLED_test/continuous_timerLED_test.ino
+++ lab1/continuous_timerLED_test/continuous_timerLED_test.ino
@@ -1,119 +1,210 @@
-#include "Timer.h" 
-Timer timer; 
-
-long number = 0; 
-int num1 = 0;
-int num2 = 0;
-int num3 = 0;
-int num4 = 0;
-int timer_event = 0;
-int numbers[4] ;
+const int red1Pin = 24; //red1 led attach to
+const int yellow1Pin = 26; //yellow1 led attach to
+const int green1Pin = 28; //green1 led attach to
+const int button = 22;//switch attach to
+const int buzzer = 30;
+bool press = false;
+bool next = false;
+int CA_1 = 7;
+int CA_2 = 8;
+int CA_3 = 9;
+int CA_4 = 10;
+int com = 2;
+const int STcp = 12;//Pin connected to ST_CP of 74HC595
+const int SHcp = 11;//Pin connected to SH_CP of 74HC595
+const int DS = 13; //Pin connected to DS of 74HC595
 int count = 0;
+int first_digit = 0;
+int second_digit = 0;
 
-#define NUM_OF_DIGITS 4
-int latch = 4; //74HC595  pin 9 STCP
-int cs = 5; //74HC595  pin 10 SHCP
-int data = 3; //74HC595  pin 8 DS
-int dPins[4] = {11, 10, 9, 8};
-//  DP G F E D C B A
-//0: 1 1 0 0 0 0 0 0
-//1: 1 1 1 1 1 0 0 1
-//2: 1 0 1 0 0 1 0 0
-//3: 1 0 1 1 0 0 0 0
-//4: 1 0 0 1 1 0 0 1
-//5: 1 0 0 1 0 0 1 0
-//6: 1 0 0 0 0 0 1 0
-//7: 1 1 1 1 1 0 0 0
-//8: 1 0 0 0 0 0 0 0
-//9: 1 0 0 1 0 0 0 0
-//10:1 0 0 0 1 0 0 0
-//11:1 0 0 0 0 0 1 1
-//12:1 1 0 0 0 1 1 0
-//13:1 0 1 0 0 0 0 1
-//14:1 0 0 0 0 1 1 0
-//15:1 0 0 0 1 1 1 0
-byte table[16] {B00000011, B10011111, B00100101, B00001101, B10011001, B01001001, B01000001, B00011111, B00000001, B00001001, B00010001, B11000001, B01100011, B10000101, B01100001, B01110001};
+//display 0,1,2,3,4,5,6,7,8,9
+byte datArray[16] {B11111100, B01100000, B11011010, B11110010, B01100110, B10110110, B10111110, B11100000, B11111110, B11110110, B11101110, B00111110, B10011100, B01111010, B10011110, B10001110};
 
-void setup() 
+int startTime;
+int limit;
+int remainder;
+int choice;
+
+void configTimer3()
 {
-  Serial.begin(115200);
-  pinMode(latch, OUTPUT);
-  pinMode(cs, OUTPUT);
-  pinMode(data, OUTPUT);
-  for (int j = 0; j < NUM_OF_DIGITS; j++)
-  {
-    pinMode(dPins[j], OUTPUT);
-    pinMode(dPins[j], HIGH);
-  }
+    // 16 MHz clock is used
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+    TCNT3  = 0x00;
+    
+    // Set clock prescaler to clk/1024
+    TCCR3B |= (1 << CS32) | (1 << CS30);
+    // Set CTC mode 
+    TCCR3B |= (1 << WGM32);
+    // Set counter to 1Hz tick (16MHz / 1024 = 15625)
+    OCR3A = 15625;
+    // Enable Output Compare A Match Interrupt
+    TIMSK3 = (1 << OCIE3A);
 }
-void loop() 
+
+void configTimer4()
 {
-  //Count from 0 to 9 on each digit
-  /*for (int i = 0; i < NUM_OF_DIGITS; i++) 
+    // 16 MHz clock is used
+    TCCR4A = 0x00;
+    TCCR4B = 0x00;
+    TCNT4  = 0x00;
+    
+    // Set clock prescaler to clk/1024
+    TCCR4B |= (1 << CS42) | (1 << CS40);
+    // Set CTC mode 
+    TCCR4B |= (1 << WGM42);
+    // Set counter to 1Hz tick (16MHz / 1024 = 15625)
+    OCR4A = 15625;
+    // Enable Output Compare A Match Interrupt
+    TIMSK4 = (1 << OCIE4A);
+}
+
+void setup()
+{
+  pinMode(com, OUTPUT);
+  pinMode(red1Pin, OUTPUT);  //set the redPin as an output
+  pinMode(yellow1Pin, OUTPUT);  //set the yellowPin as an output
+  pinMode(green1Pin, OUTPUT);  //set the greenPin as an output
+  pinMode(button, INPUT_PULLUP);
+  pinMode(buzzer, OUTPUT);
+  //set pins to output
+  pinMode(STcp, OUTPUT);
+  pinMode(SHcp, OUTPUT);
+  pinMode(DS, OUTPUT);
+  pinMode(CA_1, OUTPUT);
+  pinMode(CA_2, OUTPUT);
+  pinMode(CA_3, OUTPUT);
+  pinMode(CA_4, OUTPUT);
+  digitalWrite(com, HIGH);
+  digitalWrite(CA_1, HIGH);
+  digitalWrite(CA_2, HIGH);
+  digitalWrite(CA_3, HIGH);
+  digitalWrite(CA_4, HIGH);
+
+  //Clear global interrupt flag
+  cli();
+  
+  //Set Timer 3 interrupt at 1 Hz
+  //Used for red LED blink after boot before button has been pressed
+  configTimer3();
+  //Set Timer 4 interrupt at 1Hz
+  //Chooses LED color
+  configTimer4();
+  //Set global interrupt
+  sei();
+  
+  Serial.begin(9600);  // start serial port at 9600 bps:
+}
+
+void loop()
+{
+  if (digitalRead(button) == LOW) //check button press
   {
-    for (int j = 0; j < 16; j++) {
-      Display(i, j);
-      delay(500);
-    }
-  }*/
-  timer.update();
-  if (Serial.available())
-  { 
-    timer.stop(timer_event); 
-    screenOff(); 
-    String s = "9999";
-    number = (long)s.toInt(); 
-    if (number > 9999) 
-    { 
-      Serial.println("Enter a number between 0 and 9999");
-    }
-    else 
+    press = true;
+  }
+  if (press == true)
+  {
+    cli();
+    //Stop Timer 3 Start Timer 4
+    TIMSK3 = 0x00;
+    sei();
+    choice = 0;
+    while (true)
     {
-      separate(number);
-      timer_event = timer.every(1, Display);
+      digitalWrite(buzzer, LOW);
+      digitalWrite(yellow1Pin, LOW);
+      digitalWrite(green1Pin, LOW);
+      digitalWrite(red1Pin, HIGH);
+      next = false;
+      startTime = millis();
+      while(next == false)
+      {
+          limit = 20000;
+          remainder = (limit - (millis() - startTime)) / 1000;
+          if(remainder <= 65535  && remainder >= 0)
+          {
+            twodigit(remainder);
+          }
+          if(remainder <= 3)
+          {
+            digitalWrite(buzzer, HIGH);
+          }
+      }
+      digitalWrite(buzzer, LOW);
+      digitalWrite(red1Pin, LOW);
+      digitalWrite(green1Pin,HIGH);
+      digitalWrite(yellow1Pin, LOW);
+      next = false;
+      startTime = millis();
+      while(next == false)
+      {
+          limit = 20000;
+          remainder = (limit - (millis() - startTime)) / 1000;
+          if(remainder <= 65535 && remainder >= 0)
+          {
+            twodigit(remainder);
+          }
+          if(remainder <= 3)
+          {
+              digitalWrite(buzzer, HIGH);
+          }
+      }
+      digitalWrite(buzzer, LOW);
+      digitalWrite(red1Pin, LOW);
+      digitalWrite(green1Pin, LOW);
+      digitalWrite(yellow1Pin, HIGH);
+      next = false;
+      startTime = millis();
+      while(next == false)
+      {
+          limit = 6000;
+          remainder = (limit - (millis() - startTime)) / 1000;
+          if(remainder <= 65535 && remainder >= 0)
+          {
+            twodigit(remainder);
+          }
+          if(remainder <= 3)
+          {
+            digitalWrite(buzzer, HIGH);
+          }
+      }
     }
   }
 }
 
-void separate(long num)
-{ 
-  num1 = num / 1000;
-  numbers[0] = num1;
-  int num1_remove = num - (num1 * 1000);
-  num2 = num1_remove / 100;
-  numbers[1] = num2;
-  int num2_remove = num1_remove - (num2 * 100);
-  num3 = num2_remove / 10;
-  numbers[2] = num3;
-  num4 = num2_remove - (num3 * 10);
-  numbers[3] = num4;
+void segwrite(int number){
+  digitalWrite(STcp, LOW);
+  shiftOut(DS, SHcp, LSBFIRST, datArray[number]);
+  digitalWrite(STcp, HIGH);
 }
 
-void Display(int id, unsigned char num)
-{
-  /*digitalWrite(latch, LOW);
-  shiftOut(data, cs, MSBFIRST, table[num]);
-  digitalWrite(latch, HIGH);
-  for (int j = 0; j < NUM_OF_DIGITS; j++) 
-  {
-    digitalWrite(dPins[j], LOW);
-    digitalWrite(dPins[id], HIGH);
-  }*/
-  screenOff(); 
-  digitalWrite(latch, LOW); 
-  shiftOut(data, cs,LSBFIRST, table[numbers[count]]); 
-  digitalWrite(dPins[count], LOW); 
-  digitalWrite(latch, HIGH); 
-  count++; 
-  if (count == 4) { 
-    count = 0;
-  }
+void onedigit(int which, int value){
+  digitalWrite(which, LOW);
+  segwrite(value);
+  delay(10);
+  digitalWrite(which,HIGH);
 }
 
-void screenOff()
+void twodigit(int value)
 {
-  for(int i = 0; i < NUM_OF_DIGITS; i++)
-  {
-    digitalWrite(dPins[i], HIGH);
-  }
+  int digit0=value/10;
+  int digit1=value-(digit0 * 10);
+  
+  onedigit(CA_1,digit0);
+  onedigit(CA_2,digit1);
+}
+
+// Timer 3 ISR
+ISR(TIMER3_COMPA_vect)
+{
+    digitalWrite(red1Pin, !digitalRead(red1Pin));
+}
+
+// Timer 4 ISR
+ISR(TIMER4_COMPA_vect)
+{
+    if((millis() - startTime) >= limit)
+    {
+      next = true;
+    }
 }
