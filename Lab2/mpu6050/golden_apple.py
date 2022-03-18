b8b070804385373e0abaf6419bcaff01848341ad
StevenTran11 <98437341+StevenTran11@users.noreply.github.com>
2022-03-18 10:54:23 -0400

    Added WASD

--- Lab2/mpu6050/golden_apple.py
+++ Lab2/mpu6050/golden_apple.py
@@ -7,10 +7,8 @@ import serial
 # Note the serial port dev file name
 # need to change based on the particular host machine
 # TODO uncomment the following two lines to initialize serial port
-#serialDevFile = '/dev/cu.usbmodem14201'
-#ser=serial.Serial(serialDevFile, 9600, timeout=0)
 
-serialDevFile = 'COM3'
+serialDevFile = 'COM4'
 ser=serial.Serial(serialDevFile, 9600, timeout=0)
 
 
@@ -44,13 +42,6 @@ food_rd.shape("circle")
 food_rd.color("red")
 food_rd.penup()
 food_rd.goto(0,100)
-
-
-
-
-
-
-
 
 segments = []
 
@@ -98,10 +89,6 @@ def move():
         x = head.xcor()
         head.setx(x + 20)
         
-        
-        
-        
-        
 def shaken():
     food_rd.color("gold")
      
@@ -121,15 +108,18 @@ while True:
     wn.update()
     read = ser.readline()
     mpu_read = read.decode()
-    shake = mpu_read.rstrip()
-    if shake == 'j':
+    controller = mpu_read.rstrip()
+    if controller == 'j':
         shaken()
-       
-        
-        
-        
-   
-
+    if controller == 'w':
+        go_up()
+    elif controller == 's':
+        go_down()
+    elif controller == 'a':
+        go_left()
+    elif controller == 'd':
+        go_right()
+    
     # Check for a collision with the border
     if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
         time.sleep(1)
@@ -178,9 +168,9 @@ while True:
         # Shorten the delay
             delay -= 0.001
             
-        # Increase the score
+        # Increase the score and write to serial.
             score += 10
-            
+            ser.write(b'E')
             if score > high_score:
                 high_score = score
         
